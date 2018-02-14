273b7534c70c078c4fcdc9a536a935de41ab415e
tloy1966 <tloy1966@gmail.com>
2018-02-15 02:08:11 +0800

    加入專案檔案。

---
+++ Day8_TrackTheMario/MainForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Day8_TrackTheMario
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vspMain = new Accord.Controls.VideoSourcePlayer();
+            this.pbThreshold = new System.Windows.Forms.PictureBox();
+            this.pbMasked = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbThreshold)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMasked)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // vspMain
+            // 
+            this.vspMain.Location = new System.Drawing.Point(0, -2);
+            this.vspMain.Name = "vspMain";
+            this.vspMain.Size = new System.Drawing.Size(793, 468);
+            this.vspMain.TabIndex = 0;
+            this.vspMain.Text = "videoSourcePlayer1";
+            this.vspMain.VideoSource = null;
+            this.vspMain.NewFrame += new Accord.Controls.VideoSourcePlayer.NewFrameHandler(this.vspMain_NewFrame);
+            // 
+            // pbThreshold
+            // 
+            this.pbThreshold.Location = new System.Drawing.Point(0, 472);
+            this.pbThreshold.Name = "pbThreshold";
+            this.pbThreshold.Size = new System.Drawing.Size(405, 218);
+            this.pbThreshold.TabIndex = 1;
+            this.pbThreshold.TabStop = false;
+            // 
+            // pbMasked
+            // 
+            this.pbMasked.Location = new System.Drawing.Point(408, 472);
+            this.pbMasked.Name = "pbMasked";
+            this.pbMasked.Size = new System.Drawing.Size(382, 218);
+            this.pbMasked.TabIndex = 2;
+            this.pbMasked.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(831, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1239, 693);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pbMasked);
+            this.Controls.Add(this.pbThreshold);
+            this.Controls.Add(this.vspMain);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbThreshold)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMasked)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Accord.Controls.VideoSourcePlayer vspMain;
+        private System.Windows.Forms.PictureBox pbThreshold;
+        private System.Windows.Forms.PictureBox pbMasked;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Day8_TrackTheMario/MainForm.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Accord.Video.FFMPEG;
+using Accord.Math;
+using Accord.Math.Geometry;
+using Accord.Imaging;
+using Accord.Imaging.Filters;
+using Accord.Vision.Tracking;
+namespace Day8_TrackTheMario
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            pbThreshold.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbMasked.SizeMode = PictureBoxSizeMode.StretchImage;
+            var path = $@"{Application.StartupPath}\Mario.mp4";
+            var videoSource = new VideoFileSource(path);
+            vspMain.VideoSource = videoSource;
+            vspMain.Start();
+        }
+        BlobCounter blobCounter = new BlobCounter();
+        ColorFiltering colorFiltering = new ColorFiltering();
+        Dilation dilation = new Dilation();
+        GrayscaleBT709 grayscaleBT709 = new GrayscaleBT709();
+        Threshold threshold = new Threshold(50);
+        Rectangle lastRect = new Rectangle(0,0,50,100);
+        int cntFrame = 0;
+        int missFrame = 0;
+        List<int> lstHeight = new List<int>();
+        Camshift camshift = new Camshift
+        {
+            Conservative = false,
+            Smooth = true,
+            Mode = CamshiftMode.Mixed
+        };
+
+        private void vspMain_NewFrame(object sender, ref Bitmap image)
+        {
+            cntFrame++;
+            if (cntFrame < 80)
+            {
+                return;
+            }
+            colorFiltering.Red = new Accord.IntRange(230,245);
+            colorFiltering.Green = new Accord.IntRange(200,220);
+            colorFiltering.Blue = new Accord.IntRange(150,180);
+            
+            var filteringFrame = colorFiltering.Apply(image);
+            var  afterGray = grayscaleBT709.Apply(filteringFrame);
+            
+            var thersholdedFrame = threshold.Apply(afterGray);
+            pbThreshold.Image = afterGray;
+
+            var dilatedFrame = dilation.Apply(thersholdedFrame);
+
+            ApplyMask applyMask = new ApplyMask(dilatedFrame);
+            var maskedFrame = applyMask.Apply(image);
+            var blobFrame = maskedFrame.Clone() as Bitmap;
+            pbMasked.Image = maskedFrame;
+
+            blobCounter.ProcessImage(blobFrame);
+            var info = blobCounter.GetObjectsInformation();
+            var rectangles = info.Where(w => w.Area >=200).Select(s=>s.Rectangle).ToList();
+            //var rectangles = blobCounter.GetObjectsRectangles();
+            if (rectangles.Count ==0 )
+            {
+                missFrame++;
+                return;
+            }
+            var rectangle = rectangles[0];
+            lstHeight.Add(rectangle.Y);
+
+            if (lstHeight.Count >= 2)
+            {
+                lstHeight.Remove(0);
+            }
+            
+            //if(cntFrame >=130)
+            //    lastRect = rectangle;
+            //rectangle = rectangles.Where(w => Math.Abs(lastRect.X - w.X) < 20).FirstOrDefault();
+            if (rectangle == null)
+            {
+                return;
+            }
+            Graphics g = Graphics.FromImage(image);
+            rectangle.Width = 50;
+            rectangle.Height = 100;
+            //rectangle.Y = lstHeight.Sum(s => s) / lstHeight.Count;
+            g.DrawRectangle(new Pen(Color.Yellow, 3), rectangle);
+
+            var img = UnmanagedImage.FromManagedImage(blobFrame);
+            camshift.SearchWindow = rectangle;
+            camshift.ProcessFrame(img);
+            var trackRect = camshift.TrackingObject.Rectangle;
+            if (trackRect == null)
+            {
+                return;
+            }
+            trackRect.Inflate(30,30);
+            g.DrawRectangle(new Pen(Color.Aqua, 3), trackRect);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vspMain.Stop();
+            vspMain.Start();
+        }
+    }
+}
